d8c283d25cc97180fb248e0a210da7a4d2612c27
Sara <76701761+gjuzii@users.noreply.github.com>
2021-05-26 23:01:20 -1000

    Add files via upload

---
+++ homework03/homework03/homework03/Classes/Bike.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework03.Classes
+{
+    public class Bike : Vehicle
+    {
+        public string Color { get; set; }
+        public Bike(int id, string type, int year, int batchNumber, string color) : base(id, type, year, batchNumber)
+        {
+            Color = color;
+        }
+        public override void PrintVehicle()
+        {
+            Console.WriteLine($" Year of production - {YearOfProduction} color - {Color} ");
+        }
+    }
+}
---
+++ homework03/homework03/homework03/Classes/Vehicle .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework03.Classes
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int YearOfProduction { get; set; }
+        public int BatchNumber { get; set; }
+        public Vehicle(int id, string type, int year, int batchNumber)
+        {
+            Id = id;
+            Type = type;
+            YearOfProduction = year;
+            BatchNumber = batchNumber;
+        }
+
+        public virtual void PrintVehicle()
+        {
+            Console.WriteLine($"id - {Id}, type -{Type} year of production - {YearOfProduction}");
+        }
+    }
+}
